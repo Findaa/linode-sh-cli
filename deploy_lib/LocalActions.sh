70b93100ae134c28288b29585f0b91b4ddf521a7
Michał Cop <michalcop@bntech.dev>
2023-01-08 18:06:34 +0100

    Separate local from cloud dev base script

--- deploy_lib/LocalActions.sh
+++ deploy_lib/LocalActions.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
 kubeHostCreate() {
-  populateWorkFolder 2>/dev/null
-  isError=$?
-  if [[ $isError -eq 1 ]]; then
-    err "engine start" "Can not copy terraform folder. Check if working directory exists or root folder is OK"
-    echo $(pwd)
-    exit
-  fi
-  inf "engine start" "Success - Copy terraform files to work directory"
-
-  cat dev/null >$WORKDIR/log/tf.log
-  deployKubeHost 2>&1 | tee $WORKDIR/log/tf.log
-
-}
-
-populateWorkFolder() {
-  cp -r ./tf $WORKDIR
-  cp -r ./deploy_lib $WORKDIR
+  cat dev/null > log/tf.log
+  deployKubeHost 2>&1 | tee log/tf.log
+  sed $'s/[^[:print:]\t]//g' log/tf.log
 }
 
 deployKubeHost() {
-  cd $WORKDIR/tf/engine
+  cd tf/engine
   terraform init
   terraform plan
   terraform apply -auto-approve
@@ -29,15 +15,13 @@ deployKubeHost() {
 }
 
 kubeHostDestroy() {
-  cd $WORKDIR/tf/engine
+  cd tf/engine
   terraform destroy -auto-approve
+
   isError=$?
   if [[ $isError -eq 1 ]]; then
     err "engine destroy" "Can not delete linode engine."
   else
     inf "engine destroy" "Kube host doest not exist now. Check if connected worker nodes are removed."
   fi
-  cd .. && remove -rf ./engine
-  cd ..
-  echo "current dir test: " $(pwd)
 }
--- run.sh
+++ run.sh
@@ -1,51 +1,34 @@
 #!/bin/bash
 
-. ./deploy_lib/LocalActions.sh
-. ./deploy_lib/DatabaseManager.sh
 . ./deploy_lib/Log.sh
 
-export LINODE_CLI_TOKEN="949505f61e40135f06bf04fe99c699d15b008f8ca2a6e430d437fd3b752735ab"
 WORKDIR="./work"
+export LINODE_CLI_TOKEN="949505f61e40135f06bf04fe99c699d15b008f8ca2a6e430d437fd3b752735ab"
 
-main() {
-  PS3='Please enter your choice: '
-  options=("Create kube host" "Create cluster" "Delete host" "Delete cluster" "List nodes" "Quit")
-  select opt in "${options[@]}"; do
-    case $opt in
-    "Create kube host")
-      prepareLocalEnv
-      kubeHostCreate
-      updateWorkDir
-      printOptions
-      ;;
-    "Create cluster")
-      getKubeHostIp
-      updateWorkDir
-      printOptions
-      ;;
-    "Delete host")
-      kubeHostDestroy
-      updateWorkDir
-      printOptions
-      ;;
-    "Delete cluster")
-      echo "you chose choice $REPLY which is $opt"
-      ;;
-    "List nodes")
-      printNodes
-      printOptions
-      ;;
-    "Quit")
-      break
-      ;;
-    *) echo "invalid option $REPLY"
-      ;;
-    esac
-  done
-}
+prepareLocalEnv() {
+  createWorkDir 2>/dev/null
+  isError=$?
+  if [[ $isError -eq 1 ]]; then
+    if [ -d "$WORKDIR" ]; then
+      inf "local start" "Success - Work directory already exists. Deleting its contents"
+      rm -rf $WORKDIR
+    else
+      err "local start" "Can not create work directory. Directory does not exist"
+      exit
+    fi
+  else
+    inf "local start" "Success - Create work directory"
+  fi
 
-printOptions() {
-  echo "\n1.) Create kube host 2.) Create cluster 3.) Delete host 4.) Delete cluster 5.) List nodes 6.) Quit"
+  installPython 2>&1 | tee $WORKDIR/log/local.log
+  installLinodeCli 2>&1 | tee $WORKDIR/log/local.log
+
+  sed $'s/[^[:print:]\t]//g' $WORKDIR/log/local.log
+
+  populateWorkFolder
+  cd work
+  echo "\n\n"
+  sh worker.sh
 }
 
 createWorkDir() {
@@ -56,43 +39,24 @@ createWorkDir() {
   cd ..
 }
 
-prepareLocalEnv() {
-  createWorkDir 2>/dev/null
-  isError=$?
-  if [[ $isError -eq 1 ]]; then
-    if [ -d "$WORKDIR" ]; then
-      inf "engine start" "Success - Work directory already exists. Deleting its contents"
-      rm -rf $WORKDIR
-    else
-      err "engine start" "Can not create work directory. Directory does not exist"
-      exit
-    fi
-  else
-    inf "engine start" "Success - Create work directory"
-  fi
-
-  installPython 2>&1 | tee $WORKDIR/log/local.log
-  installLinodeCli 2>&1 | tee $WORKDIR/log/local.log
-}
-
 installPython() {
   python3 --version
   isError=$?
   if [[ $isError -eq 0 ]]; then
-    inf "python3" "Python3 already installed"
+    inf "local python3" "Python3 already installed"
   else
     sudo apt update
     sudo apt install python3
-    inf "python3" "Python3 installed successfully"
+    inf "local python3" "Python3 installed successfully"
   fi
 
   pip3 --version
   isError=$?
   if [[ $isError -eq 0 ]]; then
-    inf "python3" "Pip3 already installed"
+    inf "local python3" "Pip3 already installed"
   else
     sudo apt install python3-pip
-    inf "python3" "Pip3 installed successfully"
+    inf "local python3" "Pip3 installed successfully"
   fi
 }
 
@@ -100,7 +64,7 @@ installLinodeCli() {
   linode-cli --version 2>/dev/null
   isError=$?
   if [[ $isError -eq 0 ]]; then
-    inf "linode-cli" "Linode cli already installed"
+    inf "local linode-cli" "Linode cli already installed"
     exit
   else
     pip3 install linode-cli --upgrade
@@ -110,16 +74,36 @@ installLinodeCli() {
   linode-cli --version 2>/dev/null
   isError=$?
   if [[ $isError -eq 0 ]]; then
-    inf "linode-cli" "Linode cli installed successfully"
+    inf "local linode-cli" "Linode cli installed successfully"
   else
-    err "linode-cli" "Could not install linode cli"
+    err "local linode-cli" "Could not install linode cli"
   fi
 }
 
-updateWorkDir() {
-  if [ -d "$WORKDIR" ]; then
-    saveNodesAsCsv
+populateWorkFolder() {
+  cp -r ./tf $WORKDIR
+  isError=$?
+  if [[ $isError -eq 1 ]]; then
+    err "engine start" "Can not copy terraform folder. Check if working directory exists or root folder is OK"
+    exit
   fi
+  inf "engine start" "Success - Copy terraform files to work directory"
+
+  cp -r ./deploy_lib $WORKDIR
+  isError=$?
+  if [[ $isError -eq 1 ]]; then
+    err "engine start" "Can not copy deploy_lib folder. Check if working directory exists or root folder is OK"
+    exit
+  fi
+  inf "engine start" "Success - Copy deploy_lib files to work directory"
+
+  cp ./deploy_lib/worker.sh $WORKDIR
+  isError=$?
+  if [[ $isError -eq 1 ]]; then
+    err "engine start" "Can not copy run script. Check if working directory exists or root folder is OK"
+    exit
+  fi
+  inf "engine start" "Success - Copy run script files to work directory"
 }
 
-main
+prepareLocalEnv
